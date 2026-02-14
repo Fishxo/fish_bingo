0ce26e4e9087ec1bffdb1527591ccf5d08b7b65c
The-Professor-1 <maymerlibe@gmail.com>
2026-02-14 06:34:40 +0300

    system players on by default

--- backend/api/views.py
+++ backend/api/views.py
@@ -2235,6 +2235,9 @@ def admin_users_delete(request):
     })
 # backend/api/views.py
 from django.shortcuts import render
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 def admin_dashboard(request):
-    return render(request, '../frontend_dist/index.html')
+    return render(request, os.path.join(BASE_DIR, 'frontend_dist', 'index.html'))
