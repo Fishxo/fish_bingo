150d0f7f8eb16eb4fa71920da9abab12c78cd8ee
The-Professor-1 <maymerlibe@gmail.com>
2026-02-19 06:16:26 +0300

    fake users maxed and bug fixes

--- frontend/src/components/CardSelector.vue
+++ frontend/src/components/CardSelector.vue
@@ -84,7 +84,7 @@ export default {
   cursor: pointer;
   transition: all 0.3s ease;
   position: relative;
-  margin-top: 20px;
+  margin-top: 5px;
   font-size: 15px;
   min-height: 28px;
   min-width: 28px;
